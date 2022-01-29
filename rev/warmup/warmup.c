95a61ef75bda81161a7d9bf4ad6d1299f9039c1a
probablyhamzah <probablyhamzah@gmail.com>
2022-01-29 19:19:47 +0300

    editing warmup

--- rev/warmup/warmup.c
+++ rev/warmup/warmup.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include <string.h>
+
+// shellctf{b1t_m4N1puL47i0n5_3verYwh3r3}
+
 int main()
 {
     char flag[39];
+
+    printf("enter the flag: ");
     scanf("%s", flag);
 
     if(strlen(flag) != 38)
-        printf("wrong length");
+        printf("wrong length\n");
     else
     {
-    // shellctf{b1t_m4N1puL47i0n5_3verYwh3r3}
+    
         int check[] = {460, 416, 404, 432, 432, 396, 464, 408, 492, 392, 196, 464, 380, 436, 208, 312, 196, 448, 468, 304, 208, 220, 420, 192, 440, 212, 380, 204, 472, 404, 456, 356, 476, 416, 204, 456, 204, 500};
 
         int res = 1;
