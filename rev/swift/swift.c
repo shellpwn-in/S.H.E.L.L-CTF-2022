e5759011dc65cd4e977336487f27f72c21f11068
probablyhamzah <probablyhamzah@gmail.com>
2022-02-04 22:24:40 +0300

    updated swift

--- rev/swift/swift.c
+++ rev/swift/swift.c
@@ -1,29 +1,31 @@
 #include <stdio.h>
 
 // shellctf{T4yl0r5_s3r1e5_oF_4_fUnC7i0n}
-double func(int x) 
+double func(int x)
 {
-    double sum = 0, term = ((double)x - 1) / (x + 1), step = term * term, oldSum = 1;
+    double sum = 1, oldsum = 0, term = 1;
 
-    int i = 0;
-    while(oldSum != sum)
+    int i = 1;
+    while(sum != oldsum)
     {
-        oldSum = sum;
-        sum += term / (2 * i + 1);
-        term *= step;
+        oldsum = sum;
+        term *= (double)x / i;
+        sum += term;
+        
         i++;
     }
-    
-    return 2 * sum;
+    return sum;
 }
 
-void encrypt(char key[17]) // NATURALLOGARITHM
+void encrypt(char key[13]) // EULERSNUMBER
 {
-    double vals[16];
+    double vals[12];
     
-    for(int i = 0; i < 16; i++)
+    //int offset[12] = {-2, -1, 1, 2, -3, 0, 5, -1, -1, 1, 3, 5}
+    char offset[12] = "GVKCUSIVNABM";
+    for(int i = 0; i < 12; i++)
     {
-        vals[i] = func(key[i]);
+        vals[i] = func(key[i] - offset[i]);
         printf("%f ", vals[i]);
     }
     printf("\n");
@@ -31,6 +33,7 @@ void encrypt(char key[17]) // NATURALLOGARITHM
 
 int main()
 {
-    char flag[39] = "fhxfcceq{H4el0i5_a3k1l5_aS_4_fNhT7i0y}";
+    char flag[39] = "wbppcugz{F4zp0i5_w3l1p5_sW_4_xHhO7j0r}";
     printf("%s\n%s\n%s\n", "alright here you go: ", flag, "wait, do you not want to know the key?");
+    //encrypt("EULERSNUMBER");
 }
