fc10edc1ffaaff4ab3bba0dcbb70d9528087ab0f
Sahil Dharme <86155751+ash-S26@users.noreply.github.com>
2022-08-06 22:08:07 +0530

    Add files via upload

---
+++ web/Parameter Pollution/app.py
@@ -0,0 +1,53 @@
+from datetime import datetime
+from flask import Flask,render_template,request, url_for, make_response
+import flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    try:
+        header = flask.request.headers
+        agent = header["User-Agent"]
+        agent = agent[0].upper() + agent[1:].lower()
+        timer = header["Date"]
+        t = int(timer[17:19])
+        now = datetime.now().time() # time object
+        current_time = now.strftime("%H:%M:%S")
+        if agent == "Vinod" :
+            res = make_response(render_template("Time.html",current_time=current_time))
+            res.set_cookie('77686f616d5f695f616d', '55736572')
+            try:
+                now = datetime.now().time() # time object
+                current_time = now.strftime("%H:%M:%S")
+                h = int((str(now))[0:2])
+                if  h - t > 2 :
+            
+                    verify = request.cookies.get("77686f616d5f695f616d") # whoam_i_am
+                   
+                    if verify == "41646d696e":                         # Admin
+                        return render_template('S3cRe777.html')
+                    else:
+                        return render_template('S3cRe7.html')
+                else:
+                    return render_template('Time.html',current_time=current_time)
+            except:
+                return render_template('Time.html',current_time=current_time)
+        else:
+            res = make_response(render_template("base.html"))
+            res.set_cookie('77686f616d5f695f616d', '55736572')
+            return res
+    except:
+        now = datetime.now().time() # time object
+        current_time = now.strftime("%H:%M:%S")
+        if agent == "Vinod":
+            return render_template('Time.html',current_time=current_time)
+        else:
+            res = make_response(render_template("base.html"))
+            res.set_cookie('77686f616d5f695f616d', '55736572')
+            return res
+
+
+
+if __name__ == "__main__":
+    app.run()
