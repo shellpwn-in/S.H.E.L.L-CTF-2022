28b4f757d13a99262115c3544a094d945bb4d83f
sakshigiri2002 <85958512+sakshigiri2002@users.noreply.github.com>
2022-08-11 20:08:51 +0530

    Create encryption.py

---
+++ crypto/OX9OR2/encryption.py
@@ -0,0 +1,14 @@
+def xor(msg, key):
+    o = ''
+    for i in range(len(msg)):
+        o += chr(ord(msg[i]) ^ ord(key[i % len(key)]))
+    return o
+
+with open('message', 'r') as f:
+    msg = ''.join(f.readlines()).rstrip('\n')
+
+with open('key', 'r') as k:
+    key = ''.join(k.readlines()).rstrip('\n')
+
+with open('encrypted', 'w') as fo:
+    fo.write(xor(msg, key))
